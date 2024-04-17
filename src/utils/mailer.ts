dc9c67d44ddebda3ee59ddd7b7af9b5e41ec417a
Ameen Sid <ameensid7@outlook.com>
2024-04-17 19:24:06 +0530

    Added SendEmail Function into the utils folder

---
+++ src/utils/mailer.ts
@@ -0,0 +1,56 @@
+// Import the Required Modules
+import User from "@/models/userModel";
+import nodemailer from "nodemailer";
+import { generateToken } from "./generateVerificationToken";
+
+// function to send email
+export const sendEmail = async ({ email, emailType, userId }:any) => {
+	try {
+		// generating token with unique userId
+		const token = generateToken(userId);
+
+		// checking emailType
+		if( emailType === "VERIFY" ) {
+			const updatedUser = await User.findByIdAndUpdate(userId, {
+				$set: {
+					verifyEmailToken: token,
+					verifyEmailTokenExpiry: Date.now() + 7200000, // 2 Hours
+				}
+			});
+		} else if( emailType === "RESET" ) {
+			const updatedUser = await User.findByIdAndUpdate(userId, {
+				$set: {
+					forgotPasswordToken: token,
+					forgotPasswordTokenExpiry: Date.now() + 3600000, // 1 Hour
+				}
+			});
+		}
+		
+		// creating transporter
+		const transporter = nodemailer.createTransport({
+			host: process.env.MAIL_HOST,
+			port: 2525,
+			auth: {
+			  user: process.env.MAIL_USER,	// 🔥❌
+			  pass: process.env.MAIL_PASS,	// 🔥❌
+			}
+		});
+
+		// create mail options
+		const mailOptions = {
+			from: process.env.DEFAULT_MAIL,
+			to: email,
+			subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+			html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${token}">here</a> to ${emailType === "VERIFY" ? "Verify your email" : "Reset your password"} or copy and paste the link below in your browser.<br>${process.env.DOMAIN}/verifyemail?token=${token}</p>`,
+		};
+
+		// send mail
+		const mailResponse = await transporter.sendMail(mailOptions);
+
+		// return a response
+		return mailResponse;
+
+	} catch (error:any) {
+		throw new Error(error.message);
+	}
+}
