030a88313ec9f460d07e86019abaf2f05321e27c
Ameen Sid <ameensid7@outlook.com>
2024-04-17 17:36:28 +0530

    Added Database Configuration

---
+++ src/database/dbConfig.ts
@@ -0,0 +1,39 @@
+// Import the Required Modules
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+	try {
+		// check if MONGODB_URL exists in .env file, otherwise throw an error
+		if (!process.env.MONGODB_URI) {
+			console.error("MONGODB_URI environment variable is not defined.");
+			process.exit(1);
+		}
+
+		// ###   Connecting to the Database   ###
+		mongoose.connect(process.env.MONGODB_URI);
+
+		// ###   MongoDB Connection Events   ###
+		const connection = mongoose.connection;
+
+		connection.on('connected', () => {
+			console.log("Database Connected");
+		});
+
+		connection.on('error', (err) => {
+			console.error("MongoDB connection error: " + err);
+			process.exit(1);
+		});
+
+		// Close the MongoDB connection when Node.js process exits
+		process.on('SIGINT', async () => {
+			await mongoose.connection.close();
+			console.log("MongoDB connection closed due to Node.js process termination");
+			process.exit(0);
+		});
+
+	} catch (error) {
+		console.error("Error connecting to database: ");
+		console.error(error);
+		process.exit(1);
+	}
+}
