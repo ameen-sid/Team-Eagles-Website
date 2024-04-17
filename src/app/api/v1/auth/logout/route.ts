223824c69801df3feb2211b1d14871e278e1f3c4
Ameen Sid <ameensid7@outlook.com>
2024-04-17 20:02:25 +0530

    Added Logout API

---
+++ src/app/api/v1/auth/logout/route.ts
@@ -0,0 +1,34 @@
+// Import the Required Modules
+import { connectDB } from "@/database/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+// Connect to Database
+connectDB();
+
+// Logout API
+export const PORT = async (request: NextRequest) => {
+	try {
+		// create a response
+		const response = NextResponse.json({
+			status: 200,
+			success: true,
+			message: "Logout successfully",
+		});
+
+		// removing cookies in response
+		response.cookies.set("token", "", { 
+			httpOnly: true, 
+			expires: new Date(0) 
+		});
+
+		// return a response
+		return response;
+
+	} catch (error:any) {
+		return NextResponse.json({
+			status: 500,
+			success: false,
+			error: error.message,
+		});
+	}
+}
