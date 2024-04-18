beb04112dd1a0c18e1a390dcb2bea71ef0ecade4
Ameen Sid <ameensid7@outlook.com>
2024-04-19 01:25:43 +0530

    Updated Page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // Import the Required Modules
 import { NavBar } from "@/components";
+import Hero from "./hero";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   );
 }
