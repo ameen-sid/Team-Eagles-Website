1deb8295b1932c795610a06ccf69fb86f412655a
Ameen Sid <ameensid7@outlook.com>
2024-04-20 06:30:13 +0530

    Added Home Folder in Components Folder for Only Home Page Related Components

---
+++ src/components/home/stats-card.tsx
@@ -0,0 +1,53 @@
+// Import the Required Modules
+import React from "react";
+import { Card, Typography } from "@material-tailwind/react";
+import Image from "next/image";
+
+interface StatsCardProps {
+	icon: any,
+	count: string,
+	title: string
+}
+
+export const StatsCard = ({ icon, count, title }: StatsCardProps) => {
+	return (
+		<Card 
+			color="transparent"
+			shadow={false}
+			className="items-center" 
+			placeholder={undefined} 
+			onPointerEnterCapture={undefined} 
+			onPointerLeaveCapture={undefined}		
+		>
+			<Image
+				width={30}
+				height={30}
+				src={icon}
+				alt="icon"
+			/>
+
+			<Typography
+				variant="h1"
+				color="blue-gray"
+				className="mb-2 mt-4 text-5xl" 
+				placeholder={undefined} 
+				onPointerEnterCapture={undefined} 
+				onPointerLeaveCapture={undefined}			
+			>
+				{ count }
+			</Typography>
+
+			<Typography 
+				variant="h6"
+				className="mb-2 font-normal text-blue-gray-500" 
+				placeholder={undefined} 
+				onPointerEnterCapture={undefined} 
+				onPointerLeaveCapture={undefined}			
+			>
+        		{title}
+      		</Typography>
+		</Card>
+	);
+}
+
+export default StatsCard;
