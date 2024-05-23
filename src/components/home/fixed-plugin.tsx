d912fd414fbad3e2f573a039264db18014628ace
Ameen Sid <ameensid7@outlook.com>
2024-05-24 00:00:48 +0530

    Updated Fixed-Plugin Component

--- src/components/home/fixed-plugin.tsx
+++ src/components/home/fixed-plugin.tsx
@@ -4,14 +4,18 @@
 import Image from "next/image";
 import { Button } from "@material-tailwind/react";
 
+// Function of Fixed Plugin Component
 export const FixedPlugin = () => {
 	return (
 		<a href="https://www.material-tailwind.com" target="_blank">
 			<Button
-        		color="white"
-        		size="sm"
-        		className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50"
-      		>
+				color="white"
+				size="sm"
+				className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50" 
+				placeholder={undefined} 
+				onPointerEnterCapture={undefined} 
+				onPointerLeaveCapture={undefined}      		
+			>
 				<Image
           			width={128}
           			height={128}
@@ -25,4 +29,5 @@ export const FixedPlugin = () => {
 	);
 }
 
+// Export the Fixed Plugin Component
 export default FixedPlugin;
