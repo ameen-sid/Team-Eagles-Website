ceb141cf8f73854e8b6dbcb50f4700363a51912f
Ameen Sid <ameensid7@outlook.com>
2024-05-24 00:04:46 +0530

    Added Achievements Page

---
+++ src/app/achievements/page.tsx
@@ -0,0 +1,19 @@
+// Import the Required Modules
+import React from 'react';
+
+// Function of Achievements Page
+const Achievements = () => {
+  return (
+	<div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+    	<h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
+        	Coming Soon
+    	</h1>
+    	<p className="text-white text-center text-lg mb-8">
+	        We're working hard to bring you something amazing. Stay tuned!
+    	</p>
+	</div>
+  )
+}
+
+// Export the Achievements Page 
+export default Achievements;
