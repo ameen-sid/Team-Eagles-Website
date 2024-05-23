8988b0c4a21c0c768c360c224b911b83c018b3f0
Ameen Sid <ameensid7@outlook.com>
2024-05-23 23:57:45 +0530

    Added Update Player DP API

---
+++ src/app/api/v1/admin/update-player-dp/route.ts
@@ -0,0 +1,59 @@
+// Import the Required Modules
+import { connectDB } from "@/database/dbConfig";
+import Player from "@/models/playerModel";
+import { NextRequest, NextResponse } from "next/server";
+import { cloudinaryConnect } from "@/config/cloudinary";
+import { uploadImageToCloudinary } from "@/utils/imageUploader";
+
+// Connect to Database
+connectDB();
+
+// Disable Next.js body parser to use formidable
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
+
+// Update Player Display Picture API
+export const POST = async (request: NextRequest) => {
+	try {
+		// fetch data from form data
+		const formData = await request.formData();
+		const displayPicture = formData.get("displayPicture") as unknown as File;
+		const playerId = formData.get("playerId");
+
+		// connect to the cloudinary
+		cloudinaryConnect();
+
+		// upload image to cloudinary
+		const image = await uploadImageToCloudinary({
+			file: displayPicture,
+			folder: process.env.FOLDER_NAME!,
+			height: 1000,
+			quality: 75
+		});
+
+		// update player profile image URL
+		const updatedProfile = await Player.findOneAndUpdate(
+			{ playerId: playerId },
+			{ image: image.secure_url },
+			{ new: true }
+		);
+
+		// return a response
+		return NextResponse.json({
+			status: 200,
+			success: true,
+			message: 'Image Updated successfully',
+			data: updatedProfile,
+		});
+
+	} catch(error:any) {
+		return NextResponse.json({ 
+			status: 500,
+			success: false,
+			message: error.message
+		});
+	}
+}
