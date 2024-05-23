c59922697d5bb325865d1f1ec1bbbe042f0c712d
Ameen Sid <ameensid7@outlook.com>
2024-05-23 23:47:08 +0530

    Added hashPassword Function in utils folder

---
+++ src/utils/passwordHasher.ts
@@ -0,0 +1,14 @@
+// Import the Required Modules
+import bcryptjs from "bcryptjs";
+
+// Function to Send Email
+export const hashPassword = async (password: string) => {
+	try {
+		const salt = await bcryptjs.genSalt(10);
+
+		return await bcryptjs.hash(password, salt);
+
+	} catch (error:any) {
+		throw new Error(error.message);
+	}
+}
