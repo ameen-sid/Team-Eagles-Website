3d6dd236355ba56be581b6d0db7f8fa70247f5f2
Ameen Sid <ameensid7@outlook.com>
2024-05-24 15:10:58 +0530

    Added Back To Home Button in some pages

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 // Import the Required Modules
 import React from 'react';
+import Link from 'next/link';
+import { Button } from "@material-tailwind/react";
 
 // Function of Achievements Page
 const Achievements = () => {
@@ -8,9 +12,22 @@ const Achievements = () => {
     	<h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
         	Coming Soon
     	</h1>
+
     	<p className="text-white text-center text-lg mb-8">
 	        We are working hard to bring you something amazing. Stay tuned!
     	</p>
+
+		<Link href={"/"}>
+			<Button 
+				variant="outlined"
+				className='text-white border-white'
+				placeholder={undefined} 
+				onPointerEnterCapture={undefined} 
+				onPointerLeaveCapture={undefined}			
+			>
+				Back to Home
+			</Button>
+		</Link>
 	</div>
   )
 }
